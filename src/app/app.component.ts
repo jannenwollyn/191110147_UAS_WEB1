2c6ec47f3d2735331eb1422569784f0063ebee8a
jannenwollyn <jannen.wollyn2002@gmail.com>
2022-07-04 12:00:51 +0700

    6 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,42 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent {
+  login_form: FormGroup;
+  checkboxValue: any;
+  submitForm() {
+    // for (let v in this.login_form.controls) {
+    //   this.login_form.controls[v].markAsTouched();
+    // }
+    this.markFormTouched(this.login_form);
+    if (this.login_form.valid) {
+      // You will get form value if your form is valid
+      var formValues = this.login_form.getRawValue;
 
+    } else {
+      this.login_form.controls['terms'].setValue(false);
+    }
+  };
+
+  markFormTouched(group: FormGroup | FormArray) {
+    Object.keys(group.controls).forEach((key: string) => {
+      const control = group.controls[key];
+      if (control instanceof FormGroup || control instanceof FormArray) { control.markAsTouched(); this.markFormTouched(control); }
+      else { control.markAsTouched(); };
+    });
+  };
+
+  constructor(fb: FormBuilder) {
+    this.login_form = fb.group({
+      'email': [null, Validators.compose([Validators.required, Validators.pattern(/^(\d{10}|\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3}))$/)])],
+      'password': [null, Validators.required],
+      'terms': [false]
+    });
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from "ng2-bootstrap";
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DisableControlDirective } from './Directives/disable-control.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, ModalModule.forRoot(), ReactiveFormsModule ],
+  declarations: [ AppComponent, HelloComponent, DisableControlDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
